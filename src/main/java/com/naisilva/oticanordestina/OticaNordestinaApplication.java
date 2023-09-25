2c36db9613f4e85eb66c4112c724c601409dfad6
Nailton Simões <nailtonsimoes@gmail.com>
2023-09-25 04:18:40 -0300

    commit inicial

---
+++ src/main/java/com/naisilva/oticanordestina/OticaNordestinaApplication.java
@@ -0,0 +1,13 @@
+package com.naisilva.oticanordestina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OticaNordestinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OticaNordestinaApplication.class, args);
+	}
+
+}
